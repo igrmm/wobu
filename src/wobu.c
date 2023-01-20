2c609620f5585dbf535134f3dde4d35ddfdb99b0
igrmm <igrmmdev@gmail.com>
2023-01-20 14:07:50 -0400

    remove more unused code from nuklear example

--- src/wobu.c
+++ src/wobu.c
@@ -7,59 +7,33 @@
 #define NK_SDL_RENDERER_IMPLEMENTATION
 #include "external/nuklear_sdl_renderer.h"
 
-#define WINDOW_WIDTH 1200
-#define WINDOW_HEIGHT 800
-
 int main(int argc, char *argv[])
 {
-    /* Platform */
     SDL_Window *win;
     SDL_Renderer *renderer;
     int running = 1;
-    int flags = 0;
-    float font_scale = 1;
 
-    /* GUI */
     struct nk_context *ctx;
-    struct nk_colorf bg;
 
-    /* SDL setup */
-    SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_DISABLED, "0");
     SDL_Init(SDL_INIT_VIDEO);
 
-    win = SDL_CreateWindow("Demo", SDL_WINDOWPOS_CENTERED,
-                           SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT,
-                           SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI);
+    win = SDL_CreateWindow("wobu", SDL_WINDOWPOS_CENTERED,
+                           SDL_WINDOWPOS_CENTERED, 1200, 800, SDL_WINDOW_SHOWN);
 
     if (win == NULL) {
         SDL_Log("Error SDL_CreateWindow %s", SDL_GetError());
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
-    flags |= SDL_RENDERER_ACCELERATED;
-    flags |= SDL_RENDERER_PRESENTVSYNC;
-
-    renderer = SDL_CreateRenderer(win, -1, flags);
+    renderer = SDL_CreateRenderer(
+        win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     if (renderer == NULL) {
         SDL_Log("Error SDL_CreateRenderer %s", SDL_GetError());
-        exit(-1);
+        SDL_DestroyWindow(win);
+        exit(EXIT_FAILURE);
     }
 
-    /* scale the renderer output for High-DPI displays */
-    {
-        int render_w, render_h;
-        int window_w, window_h;
-        float scale_x, scale_y;
-        SDL_GetRendererOutputSize(renderer, &render_w, &render_h);
-        SDL_GetWindowSize(win, &window_w, &window_h);
-        scale_x = (float)(render_w) / (float)(window_w);
-        scale_y = (float)(render_h) / (float)(window_h);
-        SDL_RenderSetScale(renderer, scale_x, scale_y);
-        font_scale = scale_y;
-    }
-
-    /* GUI */
     ctx = nk_sdl_init(win, renderer);
     {
         struct nk_font_atlas *atlas;
@@ -67,16 +41,11 @@ int main(int argc, char *argv[])
         struct nk_font *font;
 
         nk_sdl_font_stash_begin(&atlas);
-        font = nk_font_atlas_add_default(atlas, 13 * font_scale, &config);
+        font = nk_font_atlas_add_default(atlas, 13, &config);
         nk_sdl_font_stash_end();
-
-        /* this hack makes the font appear to be scaled down to the desired
-         * size and is only necessary when font_scale > 1 */
-        font->handle.height /= font_scale;
         nk_style_set_font(ctx, &font->handle);
     }
 
-    bg.r = 0.10f, bg.g = 0.18f, bg.b = 0.24f, bg.a = 1.0f;
     while (running) {
         /* Input */
         SDL_Event evt;
@@ -84,52 +53,15 @@ int main(int argc, char *argv[])
         while (SDL_PollEvent(&evt)) {
             if (evt.type == SDL_QUIT)
                 goto cleanup;
+
             nk_sdl_handle_event(&evt);
         }
         nk_input_end(ctx);
 
-        /* GUI */
-        if (nk_begin(ctx, "Demo", nk_rect(50, 50, 230, 250),
-                     NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE |
-                         NK_WINDOW_MINIMIZABLE | NK_WINDOW_TITLE)) {
-            enum { EASY, HARD };
-            static int op = EASY;
-            static int property = 20;
-
-            nk_layout_row_static(ctx, 30, 80, 1);
-            if (nk_button_label(ctx, "button"))
-                fprintf(stdout, "button pressed\n");
-            nk_layout_row_dynamic(ctx, 30, 2);
-            if (nk_option_label(ctx, "easy", op == EASY))
-                op = EASY;
-            if (nk_option_label(ctx, "hard", op == HARD))
-                op = HARD;
-            nk_layout_row_dynamic(ctx, 25, 1);
-            nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-
-            nk_layout_row_dynamic(ctx, 20, 1);
-            nk_label(ctx, "background:", NK_TEXT_LEFT);
-            nk_layout_row_dynamic(ctx, 25, 1);
-            if (nk_combo_begin_color(ctx, nk_rgb_cf(bg),
-                                     nk_vec2(nk_widget_width(ctx), 400))) {
-                nk_layout_row_dynamic(ctx, 120, 1);
-                bg = nk_color_picker(ctx, bg, NK_RGBA);
-                nk_layout_row_dynamic(ctx, 25, 1);
-                bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f, 0.005f);
-                bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f, 0.005f);
-                bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f, 0.005f);
-                bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f, 0.005f);
-                nk_combo_end(ctx);
-            }
-        }
-        nk_end(ctx);
-
-        SDL_SetRenderDrawColor(renderer, bg.r * 255, bg.g * 255, bg.b * 255,
-                               bg.a * 255);
+        /* Render */
+        SDL_SetRenderDrawColor(renderer, 26.0f, 46.0f, 61.0f, 255.0f);
         SDL_RenderClear(renderer);
-
         nk_sdl_render(NK_ANTI_ALIASING_ON);
-
         SDL_RenderPresent(renderer);
     }
 
@@ -138,5 +70,5 @@ cleanup:
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(win);
     SDL_Quit();
-    return 0;
+    return EXIT_SUCCESS;
 }
