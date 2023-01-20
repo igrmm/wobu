c27ba2bf42657a6a70e3a29e7ef0ea727ddaa64a
igrmm <igrmmdev@gmail.com>
2023-01-20 08:18:53 -0400

    move nuklear definitions to nk.h

---
+++ src/nk.h
@@ -0,0 +1,13 @@
+#ifndef NK_H
+#define NK_H
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#include "external/nuklear.h"
+
+#endif
--- src/wobu.c
+++ src/wobu.c
@@ -2,16 +2,9 @@
 
 #include <SDL2/SDL.h>
 
-#define NK_INCLUDE_FIXED_TYPES
-#define NK_INCLUDE_STANDARD_IO
-#define NK_INCLUDE_STANDARD_VARARGS
-#define NK_INCLUDE_DEFAULT_ALLOCATOR
-#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
-#define NK_INCLUDE_FONT_BAKING
-#define NK_INCLUDE_DEFAULT_FONT
 #define NK_IMPLEMENTATION
+#include "nk.h"
 #define NK_SDL_RENDERER_IMPLEMENTATION
-#include "external/nuklear.h"
 #include "external/nuklear_sdl_renderer.h"
 
 #define WINDOW_WIDTH 1200
