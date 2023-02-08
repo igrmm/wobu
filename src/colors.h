2fd61805adea5f347819262c13b9ba15158c434e
igrmm <igrmmdev@gmail.com>
2023-02-08 18:44:40 -0400

    add colors.h

---
+++ src/colors.h
@@ -0,0 +1,15 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define GREY                                                                   \
+    (struct nk_color)                                                          \
+    {                                                                          \
+        45, 45, 45, 255                                                        \
+    }
+#define GREEN                                                                  \
+    (struct nk_color)                                                          \
+    {                                                                          \
+        0, 200, 150, 255                                                       \
+    }
+
+#endif
