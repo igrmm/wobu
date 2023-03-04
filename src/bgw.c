c1565b700f9c6c7013ff2196bf4b92b44d312a55
igrmm <igrmmdev@gmail.com>
2023-03-04 12:46:34 -0400

    draw outer border lines on bg grid

--- src/bgw.c
+++ src/bgw.c
@@ -7,14 +7,14 @@ void bg_render(SDL_Renderer *renderer, struct app *app)
 {
     SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
 
-    int cols = app->world_size / app->tile_size;
+    int cols = app->world_size / app->tile_size + 1;
     for (int col = 0; col < cols; col++)
         SDL_RenderDrawLine(renderer, col * app->tile_size + app->bg_scroll.x,
                            app->bg_scroll.y,
                            col * app->tile_size + app->bg_scroll.x,
                            app->world_size + app->bg_scroll.y);
 
-    int rows = app->world_size / app->tile_size;
+    int rows = app->world_size / app->tile_size + 1;
     for (int row = 0; row < rows; row++)
         SDL_RenderDrawLine(renderer, app->bg_scroll.x,
                            row * app->tile_size + app->bg_scroll.y,
