a84155dfd7d7bc0325edb304676c0603de855b00
igrmm <igrmmdev@gmail.com>
2023-03-06 18:02:00 -0400

    add tiles array to map struct

--- src/map.c
+++ src/map.c
@@ -11,5 +11,11 @@ struct map *map_create(void)
     map->tile_size = 32;
     map->size = 20;
 
+    for (int i = 0; i < TILES_MAX; i++) {
+        for (int j = 0; j < TILES_MAX; j++) {
+            map->tiles[i][j].x = map->tiles[i][j].y = -1;
+        }
+    }
+
     return map;
 }
--- src/map.h
+++ src/map.h
@@ -1,9 +1,14 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include <SDL2/SDL.h>
+
+#define TILES_MAX 100
+
 struct map {
     int size;
     int tile_size;
+    SDL_Point tiles[TILES_MAX][TILES_MAX];
 };
 
 struct map *map_create(void);
