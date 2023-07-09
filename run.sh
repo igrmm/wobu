497257fc8235e7b053fc2c5f899b880987e03725
igrmm <igrmmdev@gmail.com>
2023-07-08 21:54:27 -0400

    add valgrind suppression file

--- run.sh
+++ run.sh
@@ -5,9 +5,21 @@ cd build
 if [ "$1" = "debug" ]; then
     echo "Running debug mode."
     cmake -DCMAKE_BUILD_TYPE=Debug ..
+    cmake --build .
+    valgrind \
+        --suppressions=../valgrind.sup \
+        --leak-check=full \
+        --show-reachable=yes \
+        --show-leak-kinds=all \
+        --error-limit=no \
+        --gen-suppressions=all \
+        --track-origins=yes \
+        --keep-debuginfo=yes \
+        --log-file=supdata.log \
+        ./wobu
 else
     echo "Running normal mode."
     cmake ..
+    cmake --build .
+    ./wobu
 fi
-cmake --build .
-./wobu
