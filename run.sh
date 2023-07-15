cff65120054cb87d78f1c65f8f89a6e65eb2bb43
igrmm <igrmmdev@gmail.com>
2023-07-14 21:29:33 -0400

    add sanitizers: ubsan asan

--- run.sh
+++ run.sh
@@ -4,19 +4,25 @@ mkdir -p build
 cd build
 if [ "$1" = "debug" ]; then
     echo "Running debug mode."
+    [ "$2" != "valgrind" ] && export SANITIZERS="-fsanitize=address,undefined"
     cmake -DCMAKE_BUILD_TYPE=Debug ..
     cmake --build .
-    valgrind \
-        --suppressions=../valgrind.sup \
-        --leak-check=full \
-        --show-reachable=yes \
-        --show-leak-kinds=all \
-        --error-limit=no \
-        --gen-suppressions=all \
-        --track-origins=yes \
-        --keep-debuginfo=yes \
-        --log-file=supdata.log \
+    if [ "$2" = "valgrind" ]; then
+        echo "Running VALGRIND, checkout build/supdata.log."
+        valgrind \
+            --suppressions=../valgrind.sup \
+            --leak-check=full \
+            --show-reachable=yes \
+            --show-leak-kinds=all \
+            --error-limit=no \
+            --gen-suppressions=all \
+            --track-origins=yes \
+            --keep-debuginfo=yes \
+            --log-file=supdata.log \
+            ./wobu
+    else
         ./wobu
+    fi
 else
     echo "Running normal mode."
     cmake ..
